f5004071bba3e47ad74daca100c453ff22df3824
Pascal Lau <pascal.lau91@gmail.com>
2016-03-13 01:21:26 +0100

    Multiparty

--- server.js
+++ server.js
@@ -3,61 +3,53 @@ var express = require('express');
 var path = require('path');     //used for file path
 //var fs = require('fs-extra');       //File System - for file manipulation
 
+var multiparty = require('multiparty');
 var app = express();
 var port = process.env.PORT || 1337;
 
 var azure = require('azure-storage');
 
-console.log('starting server');
-
-app.get('/', function(req, res) {
-    res.send("blablabla");
-});
-
-app.listen(port);
 
 
-/*
 app.get('/create', createSession);
 app.get('/post/:token', postImage);
 app.get('/pattern/:token', postPattern);
 app.get('/get/:token', getImage);
 
+app.listen(port);
 
 function createSession(req, res){
     res.send("la");
 	createToken(function(token){
 		res.send(token);
 	});
-            if (part.filename) {
-
-                var size = part.byteCount - part.byteOffset;
-                var name = part.filename;
-            }
+}
 
 
-            function postImage(req, image){
-                //save the image into the queue at the right job
-                var token = req.params.token;
+function postImage(req, res){
+    //save the image into the queue at the right job
+    var token = req.params.token;
 
-                app.post('/upload', function (req, res) {
-                    var blobService = azure.createBlobService();
-                    var form = new multiparty.Form();
-                    form.on('part', function(part) {
+    var blobService = azure.createBlobService();
+    var form = new multiparty.Form();
+    form.on('part', function(part) {
+        if (part.filename) {
 
-                blobService.createBlockBlobFromStream('c', name, part, size, function(error) {
-                    if (error) {
-                        res.send({ Grrr: error });
-                    }
-                });
-            } else {
-                form.handlePart(part);
-            }
-        });
-        form.parse(req);
-        res.send('OK');
+            var size = part.byteCount - part.byteOffset;
+            var name = part.filename;
+
+            blobService.createBlockBlobFromStream('distart-input', name, part, size, function(error) {
+                if (error) {
+                    res.send({ Grrr: error });
+                }
+            });
+        } else {
+            form.handlePart(part);
+        }
     });
-
+    
+    form.parse(req);
+    res.send('OK');
 }
 
 
@@ -85,4 +77,3 @@ function createToken(callback){
 	//gen token and all the information for the job
 	callback(token);
 }
-    */
