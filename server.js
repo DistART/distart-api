ffacf11571fb82ea17ae99480cc294f72ac3f1ec
Pascal Lau <pascal.lau91@gmail.com>
2016-03-13 12:07:26 +0100

    rollback

--- server.js
+++ server.js
@@ -89,10 +89,10 @@ function getImage(req, res){
     tableJob.getJob(token, function(error, result, response, job){
         if(job){
             res.status(200);
-            job.updateJobProperty(token, "outputBlobName", job.outputBlobName+".jpeg", function(error, result, response){
-                blobSvc.getBlobToStream(OUTPUT_CONTAINER, job.outputBlobName+".jpeg", res, function(error, result, response){
+       //     job.updateJobProperty(token, "outputBlobName", job.outputBlobName+".jpeg", function(error, result, response){
+                blobSvc.getBlobToStream(OUTPUT_CONTAINER, job.outputBlobName, res, function(error, result, response){
                 });
-            })
+        //    })
         } else {
             res.status(404).send("Job not found");
         }
