87dfd68865cfef7eb861893eaed440aa89a921a3
Pascal Lau <pascal.lau91@gmail.com>
2016-03-13 11:24:13 +0100

    Output container not string

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ function getImage(req, res){
 
     tableJob.getJob(token, function(error, result, response, job){
         if(job){
-            blobSvc.getBlobToStream('OUTPUT_CONTAINER', job.outputBlobName, res, function(error, result, response){
+            blobSvc.getBlobToStream(OUTPUT_CONTAINER, job.outputBlobName, res, function(error, result, response){
                 if(!error){
                    res.status(200).send("OK: "+result + " "+response);
                 }else{
